ba3ade53cd9f525340197463017c1b2d20c42012
dhruva-25 <ds23adi@herts.ac.uk>
2025-05-26 12:28:09 +0100

    updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 from pydantic import BaseModel, Field, computed_field
 from fastapi.responses import JSONResponse
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 app = FastAPI()
@@ -11,7 +11,7 @@ class Patient(BaseModel):
     name: Annotated[str, Field(..., description= 'Name of the patient')]
     city: Annotated[str, Field(..., description="City where patient is living")]
     age: Annotated[int, Field(..., gt=0, lt= 120, description= "Age of the patient")]
-    gender: Annotated[Literal['Male', 'Female', 'Others'], Field(..., description='Gender of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
     height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
     weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in Kgs')]
     
@@ -24,19 +24,26 @@ class Patient(BaseModel):
     
     @computed_field
     @property
-    
     def verdict(self) -> str:
-        if self.bmi < 18.5:
-            return "Underweight"
-        elif self.bmi < 25:
-            return "Normal"
-        
-        elif self.bmi < 30:
-            return "Overweight"
-        
-        else:
-            return "Obese" 
 
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese' 
+
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default= None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female', 'other']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
+    
+    
 def load_data():
     with open("patients.json", "r") as f:
         data = json.load(f)
@@ -117,3 +124,50 @@ def create_patient(patient: Patient):
     save_data(data)
     
     return JSONResponse(status_code=201, content= {"message": "patient created successfully"})
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_update: PatientUpdate, patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id]
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+
+    #existing_patient_info -> pydantic object -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydandic_obj = Patient(**existing_patient_info)
+    #-> pydantic object -> dict
+    existing_patient_info = patient_pydandic_obj.model_dump(exclude='id')
+
+    # add this dict to data
+    data[patient_id] = existing_patient_info
+
+    # save data
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient updated'})
+    
+    
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    
+    # load_data
+    
+    data = load_data()
+    
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+    
+    save_data(data)
+    
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
